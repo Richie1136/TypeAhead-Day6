ea0eead3fdeb1f38469d18a49f6b1735d7da0d7b
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-03-30 14:41:58 -0400

    Using regex to figure out if the city or state matches what was searched

--- main.js
+++ main.js
@@ -5,3 +5,11 @@ const cities = []
 const prom = fetch(endpoint).then(blob => blob.json())
   .then((data) => cities.push(...data))
 console.log(cities)
+
+const findMatches = (match, cities) => {
+  return cities.filter((place => {
+    // Need to figure out if the city or state matches what was searched
+    const regex = new RegExp(match, 'gi')
+    return place.city.match(regex) || place.state.match(regex)
+  }))
+}
